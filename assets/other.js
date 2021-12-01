0ed321edb843f3dafbfa917e0329f71e10bedb94
Xiaowen2024 <71858490+Xiaowen2024@users.noreply.github.com>
2021-11-30 23:18:19 -0800

    Add files via upload

--- assets/other.js
+++ assets/other.js
@@ -1,11 +1,17 @@
 var aud = document.getElementById("audio"); 
-
+var track = localStorage.getItem("track");
 function playAud() { 
   aud.play(); 
 } 
 
 function pauseAud() { 
   aud.pause(); 
+
 } 
 
-body.onclick = playAud;
+window.onscroll = function(){
+  if (track){
+   playAud();
+  }
+}
+
